28f374dc16870bdccffa9d21e7af0e12cee7b68d
KBA Allstar <hatton.tyler@gmail.com>
2025-07-10 19:54:15 -0400

    week 2 gallery homework

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+// Footer Date Info
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.getElementById("currentyear").textContent = `© ${currentYear}, Tyler Hatton, United States`;
+document.getElementById("last-modified").textContent = `Last Modified: ${lastModified}`;
+
+// Hamburger Menu Toggle
+const menuButton = document.getElementById("menu");
+const navMenu = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click", () => {
+  const isVisible = navMenu.style.display === "flex";
+  navMenu.style.display = isVisible ? "none" : "flex";
+  menuButton.innerHTML = isVisible ? "&#9776;" : "✖";
+});
